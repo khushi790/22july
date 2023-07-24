6cb31a8829eb84ac6a6a26bfff4b85e13b805f20
khushi <khushi123@gmail.com>
2023-07-24 09:25:00 +0530

    for loop examples

---
+++ 10th.c
@@ -0,0 +1,19 @@
+/* print pattern 
+	1
+	44
+	999      */
+
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for(i=1;i<=3;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			
+			printf("%d",i*i);
+		}
+		printf("\n");
+	}
+}
---
+++ 11th.c
@@ -0,0 +1,11 @@
+//prg to print the sun of 1 to 5 numbers
+#include<stdio.h>
+int main()
+{
+	int i,j,sum=0;
+	for(i=1;i<=5;i++)
+	{
+		sum+=i;
+	}
+	printf("%d",sum);
+}
